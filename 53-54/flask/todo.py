23c400e58994d8ee755a459900eb34b817b2fcd7
eistop42 <eistopacademy@gmail.com>
2025-06-23 21:53:37 +0500

    flask with db

--- 53-54/flask/todo.py
+++ 53-54/flask/todo.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template_string, render_template, request, redirect
+import json
 
 app = Flask(__name__)
 
@@ -8,8 +9,38 @@ todo = [
     {'name': 'Посмотреть фильм', 'status': 'Не выполнено'}
 ]
 
-countries_data = [{'name': 'Россия', 'capital': 'Москва', 'people': 12000000},
-             {'name': 'Китай', 'capital': 'Пекин', 'people': 23000000}]
+
+class Database():
+    def get_countries(self):
+        countries = self._load_from_file()
+        return countries
+
+    def get_country(self, name):
+        countries = self.get_countries()
+        for country in countries:
+            if country['name'] == name:
+                return country
+    def add_country(self, name, capital, population):
+        country = {
+            'name':name,
+            'capital': capital,
+            'people': population
+        }
+        countries = self.get_countries()
+        countries.append(country)
+        self._load_to_file(countries)
+
+    def delete_country(self, name):
+        pass
+
+    def _load_from_file(self):
+        with open('countries.json', encoding='utf-8') as f:
+            return json.load(f)
+    def _load_to_file(self, countries):
+        with open('countries.json', 'w', encoding='utf-8') as f:
+            return json.dump(countries, f)
+
+db = Database()
 
 @app.route('/')
 def index():
@@ -22,6 +53,7 @@ def about():
 
 @app.route('/countries')
 def countries():
+    countries_data = db.get_countries()
     return render_template('countries.html', countries=countries_data)
 
 @app.route('/countries/<string:country_name>')
@@ -29,21 +61,16 @@ def countries_detail(country_name):
     # написать логику поиска страны по имени и передать ее в шаблон
     # перебрать список стран и найти ту, что в country_name
 
-    for country in countries_data:
-        if country['name'] == country_name:
-            return render_template('countries_detail.html', country=country)
+    country = db.get_country(country_name)
+    if country:
+        return render_template('countries_detail.html', country=country)
     return 'Такой страны нет'
 
 
 @app.route('/countries/add', methods=['POST'])
 def countries_add():
     # добавить словарь request.form в общий список, но так, чтобы ключи совпадали!
-    country = {
-        'name': request.form['name'],
-        'capital': request.form['capital'],
-        'people': request.form['population']
-    }
-    countries_data.append(country)
+    db.add_country(request.form['name'], request.form['capital'], request.form['population'])
     return redirect('/countries')
 
 
