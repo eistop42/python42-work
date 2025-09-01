f062e0ba5ce445d63938e7fe6f0b84dfd46410bd
eistop42 <eistopacademy@gmail.com>
2025-09-01 22:20:36 +0500

    django filters

--- books/core/models.py
+++ books/core/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='название категории')
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     name = models.CharField(max_length=300, verbose_name='название книги')
     author = models.CharField(max_length=255, verbose_name='автор')
     price = models.IntegerField(verbose_name='цена')
+    category = models.ForeignKey(Category, verbose_name='категория', null=True, on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name = 'книга'
@@ -12,3 +24,5 @@ class Book(models.Model):
 
     def __str__(self):
         return self.name
+
+
--- books/core/views.py
+++ books/core/views.py
@@ -1,18 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Book
+from .models import Book, Category
 
 def hello(request):
-    return HttpResponse('Hello world!')
+    """Главная страница"""
+
+    return render(request, 'main.html')
 
 
 def books(request):
-    # books_list = [
-    #     {'name': 'Идиот', 'author': 'Достоевский'},
-    #     {'name': 'Война и мир', 'author': 'Толстой'}
-    # ]
+    """Страница со списком книг"""
+
     books_list = Book.objects.all()
-    context = {'books_list': books_list}
+    category_list = Category.objects.all()
+
+    category_id_from_url = request.GET.get('category')
+    if category_id_from_url:
+        print(category_id_from_url)
+        books_list = books_list.filter(category__id=category_id_from_url)
+
+    context = {'books_list': books_list, 'category_list':category_list }
 
     return render(request, 'books.html', context)
